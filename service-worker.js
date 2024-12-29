86e7ec95a19da75b34b4e55e24da82a90867b2a8
Lian Rodriguez <105043920+Lianacrobat@users.noreply.github.com>
2024-12-29 15:18:30 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+// filepath: /d:/Drive Desktop/Proyectos personales/React/app gastos personales/components/service-worker.js
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('app-cache').then(cache => {
+      return cache.addAll([
+        '/',
+        '/registro.html',
+        '/manifest.json',
+        '/icon-192x192.png',
+        '/icon-512x512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
